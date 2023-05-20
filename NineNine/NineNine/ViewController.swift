317e35ee7143abc103bc8913d160730cdf595112
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2023-05-21 01:12:53 +0900

    Chore: Xcode 개발 환경 세팅

---
+++ NineNine/NineNine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NineNine
+//
+//  Created by Jinyoung Yoo on 2023/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
