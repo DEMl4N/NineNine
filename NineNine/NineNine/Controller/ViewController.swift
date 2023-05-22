aaab0a488462a4b48aa22681139c54dfd3330eea
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2023-05-22 19:54:36 +0900

    Feat: 게임 리스트 테이블 뷰 컨트롤러 생성

--- NineNine/NineNine/Controller/ViewController.swift
+++ NineNine/NineNine/Controller/ViewController.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var ContentTitleView: UIView!
-    @IBOutlet weak var GameContentsView: UIView!
+    @IBOutlet weak var contentTitleView: UIView!
+    @IBOutlet weak var gameContentsView: UIView!
+    @IBOutlet weak var gameContentsTableView: UITableView!
+    
+    let gameData = GameData()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,14 +23,37 @@ class ViewController: UIViewController {
         addLabelStroke(targetLabel: titleLabel, strokeHexColor: "#ECB21C", strokeWidth: 5.0)
         
         setContentsViewRoundShape(cornerRadius: 20)
+        
+        gameContentsTableView.delegate = self
+        gameContentsTableView.dataSource = self
     }
     
     func setContentsViewRoundShape(cornerRadius: CGFloat) {
-        ContentTitleView.layer.cornerRadius = cornerRadius
-        ContentTitleView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        GameContentsView.layer.cornerRadius = cornerRadius
-        GameContentsView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+        contentTitleView.layer.cornerRadius = cornerRadius
+        contentTitleView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        gameContentsView.layer.cornerRadius = cornerRadius
+        gameContentsView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+    }
+    
+    /* ----- UITableViewDataSource 프로토콜 필수 구현 메소드 ------ */
+
+    // Return the number of rows for the table.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
 
+    // Provide a cell object for each row.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = gameContentsTableView.dequeueReusableCell(withIdentifier: "gameContentCell", for: indexPath) as! GameContentsTableViewCell
+        let gameTitleArray: [String] = gameData.gameTitleArray()
+        let gameImageArray: [UIImage] = gameData.gameImageArray()
+        
+        cell.gameTitleLabel.text = gameTitleArray[indexPath.row]
+        cell.gameImageView.image = gameImageArray[indexPath.row]
+        
+        return cell
+    }
+    /* ------------------------------------------------------ */
 }
 
