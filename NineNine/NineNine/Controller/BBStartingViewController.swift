699b2df55029063369e4d3dc04a45526ae57d2cb
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2023-05-30 00:53:29 +0900

    Design: 부비부비 게임시작 화면

---
+++ NineNine/NineNine/Controller/BBStartingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BBStartingViewController.swift
+//  NineNine
+//
+//  Created by Jinyoung Yoo on 2023/05/29.
+//
+
+import UIKit
+
+class BBStartingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
