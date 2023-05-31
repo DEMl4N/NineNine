c15044337b164370cdfd6f45d0a54c0f764669f1
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2023-06-01 03:02:49 +0900

    Fix: 테이블 뷰 셀 간격 띄어놓기

--- NineNine/NineNine/Controller/ViewController.swift
+++ NineNine/NineNine/Controller/ViewController.swift
@@ -9,8 +9,9 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet weak var contentTitleView: UIView!
-    @IBOutlet weak var gameContentsView: UIView!
+    @IBOutlet weak var gameListHeadView
+    : UIView!
+    @IBOutlet weak var gameListFootView: UIView!
     @IBOutlet weak var gameContentsTableView: UITableView!
     
     let gameData = GameData()
@@ -18,25 +19,37 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setContentsViewRoundShape(cornerRadius: 20)
-        
         gameContentsTableView.delegate = self
         gameContentsTableView.dataSource = self
+        
+        setViewRoundShape(cornerRadius: 20)
+        gameContentsTableView.backgroundColor = UIColor(hex: "#F4F4F4")
     }
     
-    func setContentsViewRoundShape(cornerRadius: CGFloat) {
-        contentTitleView.layer.cornerRadius = cornerRadius
-        contentTitleView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        gameContentsView.layer.cornerRadius = cornerRadius
-        gameContentsView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let selectedIndexPath = gameContentsTableView.indexPathForSelectedRow {
+            gameContentsTableView.deselectRow(at: selectedIndexPath, animated: animated)
+        }
+    }
+    
+    func setViewRoundShape(cornerRadius: CGFloat) {
+        gameListHeadView
+            .layer.cornerRadius = cornerRadius
+        gameListHeadView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        gameContentsTableView
+            .layer.cornerRadius = cornerRadius
+        gameContentsTableView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        
     }
     
     /* ----- UITableViewDataSource 프로토콜 필수 구현 메소드 ------ */
 
     // Return the number of rows for the table.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 1
     }
+    
 
     // Provide a cell object for each row.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -45,17 +58,26 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let gameTitleArray: [String] = gameData.gameTitleArray()
         let gameImageArray: [UIImage] = gameData.gameImageArray()
         
-        cell.gameTitleLabel.text = gameTitleArray[indexPath.row]
-        cell.gameImageView.image = gameImageArray[indexPath.row]
+        cell.gameTitleLabel.text = gameTitleArray[indexPath.section]
+        cell.gameImageView.image = gameImageArray[indexPath.section]
         
         return cell
     }
     
+    
     /* ------------------------------------------------------ */
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return gameData.gameTitleArray().count
+    }
     
     // 각각의 테이블 뷰 셀에 따라 다른 화면으로 이동하는 부분
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        switch indexPath.row {
+        switch indexPath.section {
         case 0: // 탭탭 테이블 뷰 셀
             moveGameStartingView(storyBoardID: "TabTabVC")
             break
