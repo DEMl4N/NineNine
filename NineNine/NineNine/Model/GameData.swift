9ce3d80d408fe2425fd8d51199a3897a6e48b3a6
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2023-06-01 03:03:28 +0900

    Refactor: 이미지 크기 조정 코드 삭제

--- NineNine/NineNine/Model/GameData.swift
+++ NineNine/NineNine/Model/GameData.swift
@@ -9,14 +9,7 @@ import UIKit
 
 struct GameData {
     private let gameTitles: [String] = ["탭탭!", "쉐킷쉐킷!", "부비부비"]
-    private var gameImages: [UIImage]
-    
-    init() {
-        let images: [UIImage] = [#imageLiteral(resourceName: "tabtab"), #imageLiteral(resourceName: "Shake"), #imageLiteral(resourceName: "swipe")]
-        let imageSize = CGSize(width: 40, height: 40)
-        
-        self.gameImages = images.map { image in resizeImage(image: image, targetSize: imageSize) }
-    }
+    private var gameImages: [UIImage] = [#imageLiteral(resourceName: "tabtab"), #imageLiteral(resourceName: "Shake"), #imageLiteral(resourceName: "swipe")]
     
     func gameTitleArray() -> [String] {
         return gameTitles
