e1447e869540758ffdb245d11a29ce46b8cc6ccd
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2023-06-01 20:41:49 +0900

    Design: 부비부비 게임 화면 디자인

---
+++ NineNine/NineNine/View/BBLaserBoardView.swift
@@ -0,0 +1,20 @@
+//
+//  BBLaserBoardView.swift
+//  NineNine
+//
+//  Created by Jinyoung Yoo on 2023/06/01.
+//
+
+import UIKit
+
+class BBLaserBoardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ NineNine/NineNine/protocols/BBLaserBoardDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  BBLaserBoardDelegate.swift
+//  NineNine
+//
+//  Created by Jinyoung Yoo on 2023/06/01.
+//
+
+import Foundation
